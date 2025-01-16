1237d3396a1835b0ccd043ec634f20042be31873
Luciano <luciano.galvao@tikiweb.com.br>
2025-01-16 17:35:25 -0300

    feat: funcao para criacao de contas (contas teste inclusas)

--- index.js
+++ index.js
@@ -27,7 +27,55 @@ function operation() {
     ])
     .then((res) => {
       const action = res["action"];
-      console.log(action);
+      if (action === "Criar Conta") {
+        createAccount();
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+// Criando conta
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"));
+  console.log(chalk.green("Defina as opções de conta a seguir"));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para a sua conta:",
+      },
+    ])
+    .then((res) => {
+      const accountName = res["accountName"];
+
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esta conta já existe, escolha outro nome!")
+        );
+        buildAccount();
+        return
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{ "balance": 0 }',
+        function (err) {
+          console.error(err);
+        }
+      );
+
+      console.log(chalk.green("Parabéns, sua conta foi criada com sucesso!"));
+      operation();
     })
     .catch((err) => console.error(err));
 }
