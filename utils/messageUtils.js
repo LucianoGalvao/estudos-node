210b1680267b81a71f6d1161a26d399e9a2ee607
Luciano <luciano.g.galvaao@gmail.com>
2025-01-20 22:56:57 -0300

    feat: messages de password

--- utils/messageUtils.js
+++ utils/messageUtils.js
@@ -5,7 +5,7 @@ const messages = {
   options: chalk.blue("O que deseja fazer?"),
   createAccount: chalk.bgGreen.black("Parabéns por escolher o nosso banco!"),
   accountName: chalk.green("Informe seu nome:"),
-  invalidAccountName: chalk.bgRed.white("O nome é necessário, favor informar"),
+  invalidAccountName: chalk.bgRed.white("O nome é obrigatório, favor informar"),
   accountNameSuccess: (name) =>
     chalk.green(`Olá ${name}, seja bem-vindo ao Accounts!`),
   generateAccountNumber: chalk.white("Gerando o número de sua conta"),
@@ -13,6 +13,12 @@ const messages = {
   accountNumber: (name, number) =>
     chalk.bgWhite.black(`${name}, o número de sua conta é: ${number}.`),
   accountPassword: chalk.green("Informe uma senha para sua conta:"),
+  invalidAccountPassword: chalk.green('Senha é obrigatória! Cancelando operação...'),
+  creatingAccount: chalk.white("Criando a sua conta"),
+  creatingAccountSuccess: (name, number) =>
+    chalk.bgGreen.black(
+      `${name}, parabéns, a sua conta ${number} já está criada e disponível para uso!`
+    ),
 };
 
 module.exports = messages;
